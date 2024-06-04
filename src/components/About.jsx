9fc1035c52d43df058304b55e119f32c703da773
“Pigassooo” <1219632665@qq.com>
2024-06-04 23:21:38 +0800

    060405

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,31 @@ import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc'
+
+
+const ServiceCard = ({index, title, icon}) => {
+    return (
+        <Tilt className="xs:w-[250px] w-full" >
+            <motion.div
+                variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+                className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+            >
+                <div
+                    options={{
+                        max: 45,
+                        scale: 1,
+                        speed: 450,
+                    }}
+                    className='bg-tertiary rounded-[20px]  py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+                >
+                    <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+                    <h3 className='text-white text-center font-bold text-[20px]'>{title}</h3>
+                </div>
+            </motion.div>
+        </Tilt>
+    )
+}
 
 const About = () => {
     return (
@@ -14,10 +39,16 @@ const About = () => {
                 <h2 className={styles.sectionHeadText}>Overview.</h2>
             </motion.div>
             <motion.p variants={fadeIn("", "", 0.1, 1)}>
-                I am a software developer with a passion for building beautiful and functional websites and applications. I have a strong background in web development and have worked on several projects in the field. I have a strong understanding of HTML, CSS, JavaScript, and React.js. I am currently looking for new opportunities to expand my skills and grow professionally.
+                I am a full-stack developer with a passion for building beautiful and functional websites and applications. I have a strong background in web development and have worked on several projects in the field. I have a strong understanding of Vue3, React, Java, Spring Boot, MySQL and Mongodb. I am currently looking for new opportunities to expand my skills and grow professionally.
             </motion.p>
+
+            <div className='mt-20 flex flex-wrap gap-10'>
+                {services.map((service, index) => (
+                    <ServiceCard key={service.title} index={index} {...service} />
+                ))}
+            </div>
         </>
     )
 }
 
-export default About
+export default SectionWrapper(About, "about")
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,273 +1,261 @@
 import {
-  mobile,
-  backend,
-  creator,
-  web,
-  javascript,
-  typescript,
-  html,
-  css,
-  reactjs,
-  redux,
-  tailwind,
-  nodejs,
-  mongodb,
-  git,
-  figma,
-  docker,
-  meta,
-  starbucks,
-  tesla,
-  shopify,
-  carrent,
-  jobit,
-  tripguide,
-  threejs,
+mobile,
+backend,
+creator,
+web,
+javascript,
+typescript,
+html,
+css,
+reactjs,
+redux,
+tailwind,
+nodejs,
+mongodb,
+git,
+figma,
+docker,
+liyang,
+nnu,
+unsw,
+carrent,
+jobit,
+tripguide,
+threejs,
 } from "../assets";
 
 export const navLinks = [
-  {
+{
     id: "about",
     title: "About",
     url:'#about'
-  },
-  {
+},
+{
     id: "work",
     title: "Work",
     url:'#work'
-  },
-  {
+},
+{
     id: "contact",
     title: "Contact",
     url:'#contact'
-  },
+},
     {
     id: "blog",
     title: "Blog",
     url:'https://pigassooo.github.io/my-blog/'
-  }
+}
 ];
 
 const services = [
-  {
+{
     title: "Web Developer",
     icon: web,
-  },
-  {
-    title: "React Native Developer",
+},
+{
+    title: "Full Stack Developer",
     icon: mobile,
-  },
-  {
+},
+{
     title: "Backend Developer",
     icon: backend,
-  },
-  {
-    title: "Content Creator",
+},
+{
+    title: "AI Enthusiast",
     icon: creator,
-  },
+},
 ];
 
 const technologies = [
-  {
+{
     name: "HTML 5",
     icon: html,
-  },
-  {
+},
+{
     name: "CSS 3",
     icon: css,
-  },
-  {
+},
+{
     name: "JavaScript",
     icon: javascript,
-  },
-  {
+},
+{
     name: "TypeScript",
     icon: typescript,
-  },
-  {
+},
+{
     name: "React JS",
     icon: reactjs,
-  },
-  {
+},
+{
     name: "Redux Toolkit",
     icon: redux,
-  },
-  {
+},
+{
     name: "Tailwind CSS",
     icon: tailwind,
-  },
-  {
+},
+{
     name: "Node JS",
     icon: nodejs,
-  },
-  {
+},
+{
     name: "MongoDB",
     icon: mongodb,
-  },
-  {
+},
+{
     name: "Three JS",
     icon: threejs,
-  },
-  {
+},
+{
     name: "git",
     icon: git,
-  },
-  {
+},
+{
     name: "figma",
     icon: figma,
-  },
-  {
+},
+{
     name: "docker",
     icon: docker,
-  },
+},
 ];
 
 const experiences = [
-  {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
+{
+    title: "Senior High School",
+    school_name: "SLZ",
+    icon: liyang,
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    date: "September 2019 - June 2022",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+    "Chinese, Mathematics, English, Japanese, Physics, Chemistry, Geography",
+    "NEMT of 600 points, Ranking 1.3w among 40.6w",
+    "Served as the Class president, overseeing and maintaining the order of the entire class.",
+    "First prize in the National High School Mathematics Competition.",
+    "Third Prize at the provincial level in the JPSTMC.",
+    "Excellence Certificate during the Nankai University Winter Camp program.",
+    "150+ hours dedicated to community volunteer work.",
     ],
-  },
-  {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
+},
+{
+    title: "First Undergraduate program",
+    school_name: "NNU",
+    icon: nnu,
     iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
+    date: "September 2022 - July 2024",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+    "GPA:81/100, the top 10% of the grade.",
+    "Overall score ranked sixth across the entire school.",
+    "Received scholarships three times.",
+    "Served as the Grade president, overseeing and maintaining the order of the entire class.",
+    "Leading a team to achieve a national-level project designation within NUIEP.",
+    "400+ hours dedicated to community volunteer work.",
     ],
-  },
+},
+{
+    title: "Second Undergraduate program",
+    school_name: "UNSW",
+    icon: unsw,
+    iconBg: "#E6DEDD",
+    date: "September 2024 - Now",
+    points: [
+    "TO BE CONTINUED",
+    ],
+}
 ];
 
 const testimonials = [
-  {
+{
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
     name: "Sara Lee",
     designation: "CFO",
     company: "Acme Co",
     image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
+},
+{
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    "I've never met a web developer who truly cares about their clients' success like Rick does.",
     name: "Chris Brown",
     designation: "COO",
     company: "DEF Corp",
     image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
+},
+{
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
     name: "Lisa Wang",
     designation: "CTO",
     company: "456 Enterprises",
     image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
+},
 ];
 
 const projects = [
-  {
+{
     name: "Car Rent",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
     tags: [
-      {
+    {
         name: "react",
         color: "blue-text-gradient",
-      },
-      {
+    },
+    {
         name: "mongodb",
         color: "green-text-gradient",
-      },
-      {
+    },
+    {
         name: "tailwind",
         color: "pink-text-gradient",
-      },
+    },
     ],
     image: carrent,
     source_code_link: "https://github.com/",
-  },
-  {
+},
+{
     name: "Job IT",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
     tags: [
-      {
+    {
         name: "react",
         color: "blue-text-gradient",
-      },
-      {
+    },
+    {
         name: "restapi",
         color: "green-text-gradient",
-      },
-      {
+    },
+    {
         name: "scss",
         color: "pink-text-gradient",
-      },
+    },
     ],
     image: jobit,
     source_code_link: "https://github.com/",
-  },
-  {
+},
+{
     name: "Trip Guide",
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
     tags: [
-      {
+    {
         name: "nextjs",
         color: "blue-text-gradient",
-      },
-      {
+    },
+    {
         name: "supabase",
         color: "green-text-gradient",
-      },
-      {
+    },
+    {
         name: "css",
         color: "pink-text-gradient",
-      },
+    },
     ],
     image: tripguide,
     source_code_link: "https://github.com/",
-  },
+},
 ];
 
 export { services, technologies, experiences, testimonials, projects };
